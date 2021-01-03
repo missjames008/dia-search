0c13ba47b1965adeeb70c55cab117e5263823cd1
Jocelyn James <jocelynjames@Jocelyns-MacBook-Pro.local>
2021-01-03 18:33:53 -0500

    Updates

---
+++ precache-manifest.53ecd5e41d72ffb76d5c688d3f858fca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59565fe2bc8cd0118daa655d3044f68",
+    "url": "/dia-search/index.html"
+  },
+  {
+    "revision": "e0974abc4d365d82af74",
+    "url": "/dia-search/static/css/main.3395645d.chunk.css"
+  },
+  {
+    "revision": "ca2f4dbd0a12478eb949",
+    "url": "/dia-search/static/js/2.7851619a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dia-search/static/js/2.7851619a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0974abc4d365d82af74",
+    "url": "/dia-search/static/js/main.e6942ee4.chunk.js"
+  },
+  {
+    "revision": "f1bc50ab59074b5b5a0b",
+    "url": "/dia-search/static/js/runtime-main.918a8efc.js"
+  },
+  {
+    "revision": "f9d27c0a255a9e84a2cfad29884351cb",
+    "url": "/dia-search/static/media/logo.f9d27c0a.png"
+  },
+  {
+    "revision": "06a523d0872c39a1a01187d7cae92342",
+    "url": "/dia-search/static/media/question_mark.06a523d0.png"
+  }
+]);
